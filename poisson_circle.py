2997c8944411ea9dd6a55ecbf2ad301165f84abc
Marte Julie <m.j.satra@fys.uio.no>
2017-09-11 12:57:50 +0200

    code runs

--- poisson_circle.py
+++ poisson_circle.py
@@ -37,32 +37,33 @@ def solvePoisson_circle(r_domain, r_hole, hole_boundary_value, M, resolution):
 
 if __name__ == "__main__":
 
+	p_solution = solvePoisson_circle(200, 6, 80, 1.14e-3, 200) 
 	plot(p_solution, interactive=True)
 
-	Nx = 251
-	Ny = 251
-	x = linspace(-125, 125, Nx)
-	y = linspace(-125, 125, Ny)
-
-	X,Y = meshgrid(x,y)
-	p_grid = zeros([Nx, Ny])
-
-	for i in range(Nx):
-	    for j in range(Ny):
-		x_val = X[i,j]
-		y_val = Y[i,j]
-		point = Point(x_val, y_val)
-		try:
-		    p_val = p_solution(point)
-		    p_grid[i,j] = p_val
-		except:
-		    p_val = boundary_value
-		    p_grid[i,j] = boundary_value
-
-	r = np.sqrt(X**2 + Y**2)
-	import matplotlib.pyplot as plt
-	plt.imshow(p_grid)
-	plt.show()
-
-	filename = "oneVesselReady2Go.mat"
-	sio.savemat(filename, {'P':p_grid, 'R_ves':r_vessel, 'M_true':C, 'P_ves':boundary_value, 'Hx':x, 'Hy':y, 'r':r})
+#	Nx = 251
+#	Ny = 251
+#	x = linspace(-125, 125, Nx)
+#	y = linspace(-125, 125, Ny)
+#
+#	X,Y = meshgrid(x,y)
+#	p_grid = zeros([Nx, Ny])
+#
+#	for i in range(Nx):
+#	    for j in range(Ny):
+#		x_val = X[i,j]
+#		y_val = Y[i,j]
+#		point = Point(x_val, y_val)
+#		try:
+#		    p_val = p_solution(point)
+#		    p_grid[i,j] = p_val
+#		except:
+#		    p_val = boundary_value
+#		    p_grid[i,j] = boundary_value
+#
+#	r = np.sqrt(X**2 + Y**2)
+#	import matplotlib.pyplot as plt
+#	plt.imshow(p_grid)
+#	plt.show()
+#
+#	filename = "oneVesselReady2Go.mat"
+#	sio.savemat(filename, {'P':p_grid, 'R_ves':r_vessel, 'M_true':C, 'P_ves':boundary_value, 'Hx':x, 'Hy':y, 'r':r})
