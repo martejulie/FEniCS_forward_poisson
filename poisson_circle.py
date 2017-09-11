0dfd0478d4e3c4fb99e78290937b97345080c0f9
Marte Julie <m.j.satra@fys.uio.no>
2017-09-11 12:04:02 +0200

    defined solvePoisso_circle function

--- poisson_circle.py
+++ poisson_circle.py
@@ -3,41 +3,41 @@ from fenics import *
 from mshr import *
 import scipy.io as sio
 
-r_domain = 200
-r_vessel = 6
+def solvePoisson_circle(r_domain, r_hole, hole_boundary_value, M, resolution):
+	"""
+	Docstring
+	"""
 
-big_circle = Circle(Point(0,0), r_domain)
-small_circle = Circle(Point(0,0), r_vessel)
+	big_circle = Circle(Point(0,0), r_domain)
+	small_circle = Circle(Point(0,0), r_hole)
 
-domain = big_circle - small_circle
+	domain = big_circle - small_circle
+	mesh = generate_mesh(domain, resolution)
 
-resolution = 800
-mesh = generate_mesh(domain, resolution)
+	V = FunctionSpace(mesh, 'CG', 1)
 
-V = FunctionSpace(mesh, 'CG', 1)
+	p = TrialFunction(V)
+	M = Constant(M)
+	v = TestFunction(V)
+	form = (inner(nabla_grad(p), nabla_grad(v)) + M*v )*dx
+	(a,L) = system(form)
 
-p = TrialFunction(V)
-C = 1.14e-3
-M = Constant(C)
-v = TestFunction(V)
-form = (inner(nabla_grad(p), nabla_grad(v)) + M*v )*dx
-(a,L) = system(form)
+	def boundary(x, on_boundary):
+	    r = np.sqrt((x[0])**2 + (x[1])**2)
+	    b = ((r < r_hole+5) and on_boundary)
+	    return b
 
-def boundary1(x, on_boundary):
-    r = np.sqrt((x[0])**2 + (x[1])**2)
-    #b = ((r < hole_radius+DOLFIN_EPS) and on_boundary)
-    b = ((r < r_vessel+5) and on_boundary)
-    return b
+	bc = DirichletBC(V, hole_boundary_value, boundary)
 
-boundary_value = 80
-bc1 = DirichletBC(V, boundary_value, boundary1)
-bcs = [bc1]
+	p_solution = Function(V)	
+	solve(a==L, p_solution, bc)
 
-p_solution = Function(V)
-solve(a==L, p_solution, bcs)
-plot(p_solution, interactive=True)
+	return p_solution
+
 
 if __name__ == "__main__":
+
+	plot(p_solution, interactive=True)
 
 	Nx = 251
 	Ny = 251
