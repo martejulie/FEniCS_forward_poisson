578abe2c43e2e5e1f0d4e66e904b1c89b369bcde
Marte Julie <m.j.satra@fys.uio.no>
2017-09-15 15:48:36 +0200

    docstring

--- fenics2nparray.py
+++ fenics2nparray.py
@@ -3,8 +3,19 @@ import numpy as np
 
 def fenics2nparray(data, boundary_value, x, y):
 	"""
+	Writes fenics solution to numpy array.
 
+	Arguments: 
+		data (fenics solution): input data 
+		boundary_value (int): if point from numpy array not found on fenics mesh, value is set to boundary_value
+		x (array): x values of numpy array
+		y (array): y values of numpy array  
+
+	Returns: 
+		data_grid (array): output data
+		r (array): sqrt(X**2 + Y**2)
 	"""
+
 	Nx = len(x)
 	Ny = len(y)	
 	X,Y = np.meshgrid(x,y)
